87b23b380a0b1ee4c5ff5eb7b9bf61c62d2db4ff
omarghandour <alex.forget50@gmail.com>
2024-03-06 15:29:04 +0200

    first commit

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn home() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(home))
-        .bind("https://omarghandour.github.io/RustBackend/")?
+        .bind("127.0.0.1:8080")?
         .run()
         .await
 }
