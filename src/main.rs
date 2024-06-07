aa0b7a44752102b8b00d125215a5f4f02c746665
Omar Ghandour <109479370+omarghandour@users.noreply.github.com>
2024-06-07 06:36:07 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn create() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(home).service(create))
-        .bind("0.0.0.0.1:8080")?
+        .bind("127.0.0.1:8080")?
         .run()
         .await
 }
